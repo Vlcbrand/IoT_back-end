36d845813d0c7a5407d59d1bd0bcb29513b70949
vlcbrand <vlcbrand@avans.nl>
2018-03-09 10:43:11 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+var express         = require('express');
+var bodyParser      = require('body-parser');
+var routes_sensor   = require('./routes_sensor');
+var settings        = require('./config.json');
+const _             = require('./database/db-connector');
+
+var app             = express();
+
+app.set('secretkey', settings.secretkey);
+app.set('username', settings.username);
+app.set('password', settings.password);
+app.set('webPort', settings.webPort);
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+// Middelware, logging voor alle request
+app.all('*', function (req, res, next) {
+    console.log(req.method + " " + req.url);
+    next();
+});
+
+// Middleware statische bestanden (HTML, CSS, images)
+app.use('/static', express.static(__dirname + '/public'));
+
+// Routing with versions
+app.use('/sensor', routes_sensor);
+
+
+// Start server
+var port = process.env.PORT || app.get('webPort');
+var server = app.listen(port, function () {
+    console.log('Listening server on port ' + server.address().port);
+});
+
+module.exports = app;
